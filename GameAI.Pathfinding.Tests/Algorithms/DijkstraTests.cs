29d8fd2c1f0f154ac0da473bf2ab78e46ef80f22
Jan Mouwes <jan.mouwes@gmail.com>
2020-02-18 11:31:11 +0100

    - Renamed test-method again to reflect its contents - Added DijkstraIterator & DijkstraVertexInfo - Dijkstra now uses DijkstraVertexInfo

--- GameAI.Pathfinding.Tests/Algorithms/DijkstraTests.cs
+++ GameAI.Pathfinding.Tests/Algorithms/DijkstraTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using GameAI.Pathfinding.Algorithms;
 using GameAI.Pathfinding.Graph;
 using NUnit.Framework;
@@ -10,7 +11,7 @@ namespace GameAI.Pathfinding.Tests.Algorithms
         public void Setup() { }
 
         [Test]
-        public void Test_Simple3VertexGraph()
+        public void Test_Algorithm_Simple3VertexGraph()
         {
             Graph.Graph graph = GraphHelper.CreateGraph_3Vertex_Simple();
 
@@ -19,10 +20,35 @@ namespace GameAI.Pathfinding.Tests.Algorithms
             Vertex vertex1 = graph.GetVertex(1);
             Vertex vertex2 = graph.GetVertex(2);
             Vertex vertex3 = graph.GetVertex(3);
-            
+
             Assert.AreEqual(null, result.Results[vertex1].Item1);
             Assert.AreEqual(vertex1, result.Results[vertex2].Item1);
             Assert.AreEqual(vertex2, result.Results[vertex3].Item1);
+        }
+
+        [Test]
+        public void Test_Iterator_Simple3VertexGraph()
+        {
+            Graph.Graph graph = GraphHelper.CreateGraph_3Vertex_Simple();
+
+            Vertex vertex1 = graph.GetVertex(1);
+            Vertex vertex2 = graph.GetVertex(2);
+            Vertex vertex3 = graph.GetVertex(3);
+
+            DijkstraIterator iterator = new DijkstraIterator(vertex1);
+
+            Dictionary<Vertex, (Vertex, double)> results = new Dictionary<Vertex, (Vertex, double)>();
+
+            while (iterator.MoveNext())
+            {
+                (Vertex vertex, (Vertex, double) previousDistance) = iterator.Current;
+
+                results[vertex] = previousDistance;
+            }
+
+            Assert.AreEqual(null, results[vertex1].Item1);
+            Assert.AreEqual(vertex1, results[vertex2].Item1);
+            Assert.AreEqual(vertex2, results[vertex3].Item1);
         }
     }
 }
--- GameAI.Pathfinding/Algorithms/Dijkstra.cs
+++ GameAI.Pathfinding/Algorithms/Dijkstra.cs
@@ -7,30 +7,6 @@ namespace GameAI.Pathfinding.Algorithms
 {
     public class Dijkstra
     {
-        private class VertexInfo : IComparable<VertexInfo>
-        {
-            public double Distance { get; set; } = double.PositiveInfinity;
-
-            public Vertex Previous { get; set; }
-
-            public Vertex Vertex { get; set; }
-
-            public bool Known { get; set; }
-
-            public VertexInfo(Vertex vertex)
-            {
-                this.Vertex = vertex;
-            }
-
-            public int CompareTo(VertexInfo other)
-            {
-                if (ReferenceEquals(this, other)) return 0;
-                if (ReferenceEquals(null, other)) return 1;
-
-                return this.Distance.CompareTo(other.Distance);
-            }
-        }
-
         public struct DijkstraResult
         {
             public Dictionary<Vertex, (Vertex, double)> Results { get; }
@@ -38,28 +14,28 @@ namespace GameAI.Pathfinding.Algorithms
             public DijkstraResult(Dictionary<Vertex, (Vertex, double)> results) => this.Results = results;
         }
 
-        private readonly Dictionary<Vertex, VertexInfo> vertexMap = new Dictionary<Vertex, VertexInfo>();
+        private readonly Dictionary<Vertex, DijkstraVertexInfo> vertexMap = new Dictionary<Vertex, DijkstraVertexInfo>();
 
-        private VertexInfo GetVertexInfo(Vertex vertex)
+        private DijkstraVertexInfo GetDijkstraVertexInfo(Vertex vertex)
         {
-            if (!this.vertexMap.ContainsKey(vertex)) { this.vertexMap[vertex] = new VertexInfo(vertex); }
+            if (!this.vertexMap.ContainsKey(vertex)) { this.vertexMap[vertex] = new DijkstraVertexInfo(vertex); }
 
             return this.vertexMap[vertex];
         }
 
-        private PriorityQueue<VertexInfo> queue = new PriorityQueue<VertexInfo>();
+        private PriorityQueue<DijkstraVertexInfo> queue = new PriorityQueue<DijkstraVertexInfo>();
 
         public DijkstraResult Run(Vertex origin)
         {
-            VertexInfo originVertexInfo = GetVertexInfo(origin);
-            originVertexInfo.Distance = 0;
-            this.queue.Add(originVertexInfo);
+            DijkstraVertexInfo originDijkstraVertexInfo = GetDijkstraVertexInfo(origin);
+            originDijkstraVertexInfo.Distance = 0;
+            this.queue.Add(originDijkstraVertexInfo);
 
             while (this.queue.Size > 0)
             {
-                VertexInfo currentVertex = this.queue.Remove();
+                DijkstraVertexInfo currentVertex = this.queue.Remove();
 
-                bool isFarther = currentVertex.Distance > GetVertexInfo(currentVertex.Vertex).Distance;
+                bool isFarther = currentVertex.Distance > GetDijkstraVertexInfo(currentVertex.Vertex).Distance;
 
                 if (currentVertex.Known || isFarther) { continue; }
 
@@ -69,7 +45,7 @@ namespace GameAI.Pathfinding.Algorithms
 
                 foreach (Edge edge in currentVertex.Vertex.Edges)
                 {
-                    VertexInfo vertexInfo = new VertexInfo(edge.dest)
+                    DijkstraVertexInfo vertexInfo = new DijkstraVertexInfo(edge.dest)
                     {
                         Distance = currentVertex.Distance + edge.cost,
                         Previous = currentVertex.Vertex
@@ -81,7 +57,7 @@ namespace GameAI.Pathfinding.Algorithms
 
             Dictionary<Vertex, (Vertex, double)> results = new Dictionary<Vertex, (Vertex, double)>();
 
-            foreach ((Vertex vertex, VertexInfo vertexInfo) in this.vertexMap) { results[vertex] = (vertexInfo.Previous, vertexInfo.Distance); }
+            foreach ((Vertex vertex, DijkstraVertexInfo vertexInfo) in this.vertexMap) { results[vertex] = (vertexInfo.Previous, vertexInfo.Distance); }
 
             return new DijkstraResult(results);
         }
