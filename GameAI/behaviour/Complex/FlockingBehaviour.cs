6ce999aba5d08b2607912a290a45dab59557a706
martschilt <mart.schilthuis@knmi.nl>
2020-03-04 13:17:42 +0100

    added rock

--- GameAI/behaviour/Complex/FlockingBehaviour.cs
+++ GameAI/behaviour/Complex/FlockingBehaviour.cs
@@ -9,12 +9,22 @@ namespace GameAI.behaviour.Complex
     {
         public World World;
         private readonly double radius;
+        public int separation;
+        public int alignment;
+        public int cohesion;
 
-        public FlockingBehaviour(MovingEntity entity, World world, double radius) : base(entity)
+        public FlockingBehaviour(MovingEntity entity, World world, double radius, int separation, int alignment,
+            int cohesion) : base(entity)
         {
             this.World = world;
             this.radius = radius;
+            this.separation = separation;
+            this.alignment = alignment;
+            this.cohesion = cohesion;
         }
+
+        public FlockingBehaviour(MovingEntity entity, World world, double radius) : this(entity, world, radius, 1, 2, 5)
+        { }
 
         public override Vector2 Calculate()
         {
@@ -28,9 +38,9 @@ namespace GameAI.behaviour.Complex
             // Check whether there are any entities nearby
             IEnumerable<MovingEntity> neighbors = this.World.entities.Where(IsNear);
 
-            Vector2 target = (SteeringBehaviours.Separation(this.Entity, neighbors) * 1) + 
-                             (SteeringBehaviours.Alignment(this.Entity, neighbors) * 3) +
-                             (SteeringBehaviours.Cohesion(this.Entity, neighbors) * 2);
+            Vector2 target = (SteeringBehaviours.Separation(this.Entity, neighbors) * separation) + 
+                             (SteeringBehaviours.Alignment(this.Entity, neighbors) * alignment) +
+                             (SteeringBehaviours.Cohesion(this.Entity, neighbors) * cohesion);
             
             if (target.Equals(Vector2.Zero))
             {
---
+++ GameAI/entity/Rock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameAI.entity
+{
+    class Rock : BaseGameEntity
+    {
+        public Vector2 Pos { get; set; }
+        public float Scale { get; set; }
+        public World World { get; set; }
+
+        public Rock(Vector2 pos, World w) : base(pos, w)
+        { }
+
+        public override void Update(GameTime gameTime)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
