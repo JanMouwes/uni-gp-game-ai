dc21dce00281e0160fe552e4e3496e0d289a48ec
martschilt <mart.schilthuis@knmi.nl>
2020-03-31 15:52:32 +0200

    pathsmoothing basic edition

--- GameAI/Navigation/DefaultPathSmoother.cs
+++ GameAI/Navigation/DefaultPathSmoother.cs
@@ -9,6 +9,27 @@ namespace GameAI.Navigation
         /// Does not smooth paths
         /// </summary>
         /// <inheritdoc cref="IPathSmoother#SmoothPath"/>
-        public IEnumerable<Vector2> SmoothPath(IEnumerable<Vector2> path) => path;
+        public IEnumerable<Vector2> SmoothPath(LinkedList<Vector2> path)
+        {
+            // First remove points with the first node as base point
+            LinkedListNode<Vector2> node = path.First;
+            for (int i = 0; i < path.Count; i++)
+            {
+                // Checking if this is the last item in the linkedlist
+                if (node.Next == null) break;
+                LinkedListNode<Vector2> one = node.Next;
+                // Checking to see if one is the last item in the linkedlist
+                if (one.Next == null) break;
+                LinkedListNode<Vector2> two = one.Next;
+
+                // Checking if the distance from the source to the second next node is faster by approaching it directly
+                float distOriginal = Vector2.DistanceSquared(node.Value, one.Value) + Vector2.DistanceSquared(one.Value, two.Value);
+                float distNew = Vector2.DistanceSquared(node.Value, two.Value);
+                if (distNew <= distOriginal) path.Remove((one));
+                
+                node = node.Next;
+            }
+            return path;
+        }
     }
 }
