c56cd9f00a1249b489b278804b51e642838bc087
MartSchilt <schilthuismart@gmail.com>
2020-04-22 20:29:02 +0200

    added bird.cs

---
+++ GameAI/entity/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameAI.Entity.GoalBehaviour.Composite;
+using GameAI.world;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameAI.Entity
+{
+    class Bird : MovingEntity
+    {
+        public Color Color { get; set; }
+
+        public Bird(World world) : base(world)
+        {
+            Velocity = new Vector2(0, 0);
+            Scale = 5;
+
+            this.Color = Color.Black;
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            this.Graphics.Draw(spriteBatch);
+        }
+    }
+}
